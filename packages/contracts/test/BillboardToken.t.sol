e75f3b75271a1901591f68ccba147c9fd3fee2c3
0xJoshbillboard <0xJoshbillboard@gmail.com>
2025-05-05 17:57:29 +0100

    implemented tests

---
+++ packages/contracts/test/BillboardToken.t.sol
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BillboardToken} from "../src/BillboardToken.sol";
+import {USDCMock} from "../src/mocks/USDCMock.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+
+contract BillboardTokenTest is Test {
+    BillboardToken public token;
+    USDCMock public usdc;
+
+    address public owner;
+    address public buyer;
+    address public user2;
+    uint256 public constant TOTAL_SUPPLY = 10_000_000 * 10 ** 18;
+    uint256 public constant USDC_DECIMALS = 6;
+    uint256 public constant TOKEN_DECIMALS = 18;
+
+    function setUp() public {
+        owner = address(this);
+        buyer = address(0x1);
+        user2 = address(0x2);
+
+        usdc = new USDCMock();
+        token = new BillboardToken(address(usdc));
+
+        // Mint USDC to buyer for testing
+        usdc.mint(buyer, 1_000_000 * 10 ** USDC_DECIMALS);
+        vm.startPrank(buyer);
+        usdc.approve(address(token), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function test_Initialization() public view {
+        assertEq(token.name(), "Billboard Governance Token");
+        assertEq(token.symbol(), "BBT");
+        assertEq(token.totalSupply(), TOTAL_SUPPLY);
+        assertEq(token.balanceOf(owner), TOTAL_SUPPLY);
+        assertEq(address(token.usdc()), address(usdc));
+    }
+
+    function test_BuyTokens() public {
+        uint256 usdcAmount = 1000 * 10 ** USDC_DECIMALS;
+        uint256 expectedBbtAmount = 1000 * 10 ** TOKEN_DECIMALS;
+
+        vm.startPrank(buyer);
+        uint256 initialBalance = token.balanceOf(buyer);
+        token.buyTokens(usdcAmount);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(buyer), initialBalance + expectedBbtAmount);
+        assertEq(usdc.balanceOf(address(token)), usdcAmount);
+        assertEq(token.soldTokens(), expectedBbtAmount);
+    }
+
+    function test_BuyTokens_RevertWhenAmountZero() public {
+        vm.startPrank(buyer);
+        vm.expectRevert("Amount must be greater than 0");
+        token.buyTokens(0);
+        vm.stopPrank();
+    }
+
+    function test_BuyTokens_RevertWhenExceedsOneMillion() public {
+        uint256 usdcAmount = 1_000_001 * 10 ** USDC_DECIMALS;
+        
+        vm.startPrank(buyer);
+        // The ERC20 balance check will fail before our custom check
+        vm.expectRevert(abi.encodeWithSelector(
+            IERC20Errors.ERC20InsufficientBalance.selector,
+            buyer,
+            1_000_000 * 10 ** USDC_DECIMALS,
+            1_000_001 * 10 ** USDC_DECIMALS
+        ));
+        token.buyTokens(usdcAmount);
+        vm.stopPrank();
+    }
+
+    function test_BuyTokens_RevertWhenTransferFails() public {
+        uint256 usdcAmount = 1000 * 10 ** USDC_DECIMALS;
+        
+        vm.startPrank(buyer);
+        usdc.approve(address(token), 0); // Revoke approval
+        vm.expectRevert(abi.encodeWithSelector(
+            IERC20Errors.ERC20InsufficientAllowance.selector,
+            address(token),
+            0,
+            usdcAmount
+        ));
+        token.buyTokens(usdcAmount);
+        vm.stopPrank();
+    }
+
+    function test_WithdrawUSDC() public {
+        uint256 usdcAmount = 1000 * 10 ** USDC_DECIMALS;
+        
+        // First buy some tokens to get USDC in the contract
+        vm.startPrank(buyer);
+        token.buyTokens(usdcAmount);
+        vm.stopPrank();
+
+        uint256 initialBalance = usdc.balanceOf(owner);
+        token.withdrawUSDC(usdcAmount);
+        uint256 finalBalance = usdc.balanceOf(owner);
+
+        assertEq(finalBalance - initialBalance, usdcAmount);
+        assertEq(usdc.balanceOf(address(token)), 0);
+    }
+
+    function test_WithdrawUSDC_RevertWhenAmountZero() public {
+        vm.expectRevert("Amount must be greater than 0");
+        token.withdrawUSDC(0);
+    }
+
+    function test_WithdrawUSDC_RevertWhenNotOwner() public {
+        uint256 usdcAmount = 1000 * 10 ** USDC_DECIMALS;
+        
+        vm.startPrank(buyer);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, buyer));
+        token.withdrawUSDC(usdcAmount);
+        vm.stopPrank();
+    }
+
+    function test_WithdrawUSDC_RevertWhenInsufficientBalance() public {
+        uint256 usdcAmount = 1000 * 10 ** USDC_DECIMALS;
+        
+        vm.expectRevert(abi.encodeWithSelector(
+            IERC20Errors.ERC20InsufficientBalance.selector,
+            address(token),
+            0,
+            usdcAmount
+        ));
+        token.withdrawUSDC(usdcAmount);
+    }
+
+    function test_MultipleBuys() public {
+        uint256 usdcAmount1 = 500 * 10 ** USDC_DECIMALS;
+        uint256 usdcAmount2 = 300 * 10 ** USDC_DECIMALS;
+        uint256 expectedTotal = 800 * 10 ** TOKEN_DECIMALS;
+
+        vm.startPrank(buyer);
+        token.buyTokens(usdcAmount1);
+        token.buyTokens(usdcAmount2);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(buyer), expectedTotal);
+        assertEq(token.soldTokens(), expectedTotal);
+        assertEq(usdc.balanceOf(address(token)), usdcAmount1 + usdcAmount2);
+    }
+
+    function test_BuyTokens_ExactOneMillion() public {
+        uint256 usdcAmount = 1_000_000 * 10 ** USDC_DECIMALS;
+        uint256 expectedBbtAmount = 1_000_000 * 10 ** TOKEN_DECIMALS;
+
+        vm.startPrank(buyer);
+        token.buyTokens(usdcAmount);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(buyer), expectedBbtAmount);
+        assertEq(token.soldTokens(), expectedBbtAmount);
+        assertEq(usdc.balanceOf(address(token)), usdcAmount);
+    }
+} 
